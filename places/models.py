26991f06044008b568ccae020c33e0de3fbcea3b
vladimir-asus <vladimir@vladimir-e200ha>
2022-03-05 11:01:27 +0500

    Add model for images

--- places/models.py
+++ places/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 
 
+def media_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/placeId/<filename>
+    return '{0}/{1}'.format(instance.place.placeId, filename)
+
+
 class Place(models.Model):
     title = models.CharField(max_length=200)
-    placeId = models.CharField(max_length=200)
+    placeId = models.CharField(max_length=200, unique=True)
     lat = models.FloatField()
     lon = models.FloatField()
     description_short = models.CharField(max_length=256, blank=True)
@@ -11,3 +16,13 @@ class Place(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Image(models.Model):
+    place = models.ForeignKey(
+        Place, related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField(
+        null=True, blank=True, upload_to=media_directory_path)
+
+    def __str__(self):
+        return str(self.id) + ' ' + self.place.title
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from . import views
-  
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -31,3 +31,5 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(
         settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
