3a7b8eae6ac3e4208b6899478db9f6e4d0aab60f
vladimir-asus <vladimir@vladimir-E200HA>
2023-12-20 17:02:19 +0500

    Add blank start page

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.shortcuts import render
+
+
+def show_dummy(request):
+    return render(request, 'index.html')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', show_dummy),
 ]
