1680f9ff6bb45716a93d1017c01117b3f25dfef4
Vladimir <Vladimir@DESKTOP-D3T5842>
2023-12-31 10:40:31 +0500

    Add Place model

--- places/admin.py
+++ places/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Place, Image
 
-# Register your models here.
+
+class ImageInline(admin.TabularInline):
+    model = Image.places.through
+    extra = 1
+
+
+@admin.register(Place)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('title', 'coordinates',)
+    inlines = [ImageInline]
+    search_fields = ['title']
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'image',)
--- places/models.py
+++ places/models.py
@@ -1,1 +1,21 @@
-print('hello world')
+from django.db import models
+
+
+class Place(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Название места')
+    description_short = models.TextField(verbose_name='Краткое описание')
+    description_long = models.TextField(verbose_name='Полное описание')
+    coordinates = models.JSONField(verbose_name='Географические координаты')
+    imgs = models.ManyToManyField('Image', related_name='places',
+                                  verbose_name='Фотографии')
+
+    def __str__(self):
+        return self.title
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='place_imgs/',
+                              verbose_name='Фотография')
+
+    def __str__(self):
+        return f'Фото {self.pk}'
