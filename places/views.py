913b93b67aeb340b740b52d2234ca2424fc1de00
vladimir-asus <vladimir@vladimir-E200HA>
2024-01-03 01:29:16 +0500

    FIXME: 404 for images

--- places/views.py
+++ places/views.py
@@ -9,23 +9,27 @@ def to_geojson(places, request):
     features = []
     for place in places:
         point = geojson.Point(tuple(map(float, place.coordinates.values())))
-        place_properties = {
+        detailsUrl = {
             'title': place.title,
             'description_short': place.description_short,
             'description_long': place.description_long,
             'coordinates': place.coordinates,
             'imgs': [request.build_absolute_uri(img.image.url) for img in place.imgs.all()],
         }
+        place_properties = {
+            'title': place.title,
+            'placeId': place.id,
+            'detailsUrl': detailsUrl,
+        }
         feature = geojson.Feature(geometry=point, properties=place_properties)
         features.append(feature)
-    geojson_data = geojson.dumps(geojson.FeatureCollection(features))
+    geojson_data = geojson.FeatureCollection(features)
     return geojson_data
 
 
 def index(request):
     places = Place.objects.all()
     data = to_geojson(places, request)
-    serialized_json = json.dumps(data)
-    print(data)
-    print(serialized_json)
-    return render(request, 'index.html', {'json_data': data})
+    # serialized_json = json.dumps(data)
+    print(type(data))
+    return render(request, 'index.html', {'data': data})
