899959daf7770b21d12a9d31cb682f8c0912f8d5
vladimir-kuu <vladimir-kuu@xbook2.com>
2024-01-18 19:11:41 +0500

    Upd requirements.txt

--- places/admin.py
+++ places/admin.py
@@ -10,7 +10,7 @@ from .models import Image, Place
 class SortableImageInline(SortableTabularInline):
     fields = ('file', 'get_preview', 'order')
     readonly_fields = ('get_preview',)
-    ordering = ['order']
+    ordering = ('order',)
     model = Image
     extra = 1
 
@@ -30,7 +30,7 @@ class SortablePlaceAdmin(SortableAdminBase, admin.ModelAdmin):
 class SortableImageAdmin(admin.ModelAdmin):
     list_display = ('file', 'url', 'get_preview', 'order')
     readonly_fields = ('get_preview',)
-    ordering = ['order']
+    ordering = ('order',)
 
     def get_preview(self, obj):
         return mark_safe(f'<img src="{obj.get_url}" width="200px" height="200px"/>')
