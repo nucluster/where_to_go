dd492ef411f7e7ab1baaf78e98acd28c05c871f7
vladimir-kuu <vladimir-kuu@xbook2.com>
2024-01-18 14:28:27 +0500

    Add validation to Image form

--- places/admin.py
+++ places/admin.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib import admin
 from django.db import models
 from django.utils.safestring import mark_safe
@@ -10,7 +11,7 @@ from .models import Place, Image
 class SortableImageInline(SortableTabularInline):
     fields = ('file', 'get_preview', 'order')
     readonly_fields = ('get_preview',)
-    ordering = ['order']
+    ordering = ('order',)
     model = Image
     extra = 1
 
@@ -27,11 +28,26 @@ class SortablePlaceAdmin(SortableAdminBase, admin.ModelAdmin):
     }
 
 
+class ImageForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        fields = '__all__'
+
+    def clean(self):
+        cleaned_data = super().clean()
+        url = cleaned_data.get('url')
+        file = cleaned_data.get('file')
+        if not url and not file:
+            raise forms.ValidationError("Должно быть заполнено поле 'Внешний URL' или 'Файл'")
+        return cleaned_data
+
+
 @admin.register(Image)
 class SortableImageAdmin(admin.ModelAdmin):
+    form = ImageForm
     list_display = ('file', 'url', 'get_preview', 'order')
     readonly_fields = ('get_preview',)
-    ordering = ['order']
+    ordering = ('order',)
 
     def get_preview(self, obj):
         return mark_safe(f'<img src="{obj.get_url}" width="200px" height="200px"/>')
